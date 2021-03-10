146bc2135da8d49ecf88958eb177c2df58dab17d
SUDIP MONDAL <74463091+sudip-mondal-2002@users.noreply.github.com>
2021-03-10 22:58:03 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+var date=new Date();
+var year=date.getFullYear()
+function Footer(){
+    return <footer><p>Copyright {year}</p></footer>;
+}
+
+export default Footer;
