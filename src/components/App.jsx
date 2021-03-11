899fdcf4a4ba2e3da3464b990e9468031ca524f0
SUDIP MONDAL <74463091+sudip-mondal-2002@users.noreply.github.com>
2021-03-12 00:02:29 +0530

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,55 @@
-import React from "react";
-import Header from "./Header"
-import Note from "./Note"
-import Footer from "./Footer"
-import notes from "../notes"
-
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
-    return (<div>
-        <Header></Header>
-        {notes.map(note => <Note key={note.key} title= {note.title} content= {note.content} />)}
-        <Footer></Footer>
-    </div>)
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
